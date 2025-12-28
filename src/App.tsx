4f49f3ed5dc1e781caf868e80634e5beb49efcf9
seyar_ad <ad-abzatov.seyar@mail.ru>
2025-12-28 23:20:19 +0300

    any markup continue

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@ const App = () => {
   return (
     <div className="background">
       background
-      <div className="header">header</div>
-      <div className="leftside">leftside</div>
-      <div className="rightside">rightside</div>
-      <div className="footer">footer</div>
+      <div className="body">
+        <div className="leftside">
+          <div className='photoblock'></div>
+          <div className='contactblock'>
+            <p>Ад-Абзатов Сейяр</p>
+            <p>Fullstack-разработчик</p>
+            <p>Россия, Анапа</p>
+          </div>
+        </div>
+        <div className="rightside">
+          <h3>Привет, меня зовут Сейяр Ад-Абзатов</h3>
+          <h4>Кратко о себе</h4>
+          <p>Начинающий Fullstack-разработчик без опыта коммерческой разработки. Но с опытом разработки собственных pet-проектов. Главная цель - получить позицию Junior разработчика с навыками JavaScript/TypeScript и дальнейшего профессионального роста. Мотивирован изучать новые технологии и решать новые для себя задачи.</p>
+          <h4>Мои контакты:</h4>
+        </div>
+      </div>
+      <div className="footer">©2025</div>
     </div>
   );
 }
